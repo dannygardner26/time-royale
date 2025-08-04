c1f730db9579c946e745771a1c184cea3b87cc9d
Kevin Wang <kevindakiwi6000@gmail.com>
2025-08-04 14:32:32 -0400

    units stuff

--- units.py
+++ units.py
@@ -1,21 +1,39 @@
 import pygame, sys
 from pygame.locals import*
-import Card
+import main
 
 class Units:
-    def __init__(self, card:Card, side:bool):
+    def __init__(self, id:int, image_path:str, health:int, damage:int, attackRate:int, speed:int, range:int, side:bool):
+        self.id = id
+        self.image = pygame.image.load(image_path)
+        self.health = health
+        self.damage = damage
+        self.speed = speed
+        self.range = range
         self.position = 100*side
         self.side = side
-        self.card = card
-    def update(self):
-        
-    def attack(self):
-
+        self.attackRate = attackRate
+        self.aCounter = 0
     def move(self):
-        # check to see enemy or tower
         # change position
         if self.side and self.position >= 0:
             self.position -= self.card.speed
         elif self.position <= 100:
             self.position += self.card.speed
+    def takeDamage(self, amount):
+        self.health -= amount
+    def attack(self, target):
+        self.aCounter += 1
+        if self.aCounter % self.attackRate == 0:
+            target.takeDamage(self.damage)
+    def update(self):
+        # find enemies
+        enemies = main.enemies
+        if self.side:
+            enemies = main.allies
+        for enemy in enemies: # enemies is placeholder, list of enemy units
+            if abs(enemy.position - self.position) < self.range:
+                self.attack(self, enemy)
+            else:
+                self.move(self)       
         
